0e4fa0840c804097505b69d7c05ca0b2785f90dc
Prakash Chhetri <prakash@tonic.com.au>
2022-04-03 09:43:38 +1000

    Added API Response trait

---
+++ app/Traits/ApiResponses.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+trait ApiResponses
+{
+    /**
+     * Success Response.
+     *
+     * @param  mixed  $data
+     * @param  int  $statusCode
+     * @return JsonResponse
+     */
+    public static function successResponse(mixed $data, int $statusCode = Response::HTTP_OK): JsonResponse
+    {
+        return new JsonResponse($data, $statusCode);
+    }
+
+    /**
+     * Error Response.
+     *
+     * @param  mixed  $data
+     * @param  string  $message
+     * @param  int  $statusCode
+     * @return JsonResponse
+     */
+    public static function errorResponse(
+        mixed $data,
+        string $message = '',
+        int $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR
+    ): JsonResponse {
+        if (!$message) {
+            $message = Response::$statusTexts[$statusCode];
+        }
+
+        $data = [
+            'message' => $message,
+            'errors' => $data,
+        ];
+
+        return new JsonResponse($data, $statusCode);
+    }
+
+    /**
+     * Response with status code 200.
+     *
+     * @param  mixed  $data
+     * @return JsonResponse
+     */
+    public static function okResponse(mixed $data): JsonResponse
+    {
+        return self::successResponse($data);
+    }
+
+    /**
+     * Response with status code 201.
+     *
+     * @param  mixed  $data
+     * @return JsonResponse
+     */
+    public static function createdResponse(mixed $data): JsonResponse
+    {
+        return self::successResponse($data, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Response with status code 204.
+     *
+     * @return JsonResponse
+     */
+    public static function noContentResponse(): JsonResponse
+    {
+        return self::successResponse([], Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Response with status code 400.
+     *
+     * @param  mixed  $data
+     * @param  string  $message
+     * @return JsonResponse
+     */
+    public static function badRequestResponse(mixed $data, string $message = ''): JsonResponse
+    {
+        return self::errorResponse($data, $message, Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * Response with status code 401.
+     *
+     * @param  mixed  $data
+     * @param  string  $message
+     * @return JsonResponse
+     */
+    public static function unauthorizedResponse(mixed $data, string $message = ''): JsonResponse
+    {
+        return self::errorResponse($data, $message, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Response with status code 403.
+     *
+     * @param  mixed  $data
+     * @param  string  $message
+     * @return JsonResponse
+     */
+    public static function forbiddenResponse(mixed $data, string $message = ''): JsonResponse
+    {
+        return self::errorResponse($data, $message, Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Response with status code 404.
+     *
+     * @param  mixed  $data
+     * @param  string  $message
+     * @return JsonResponse
+     */
+    public static function notFoundResponse(mixed $data, string $message = ''): JsonResponse
+    {
+        return self::errorResponse($data, $message, Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * Response with status code 409.
+     *
+     * @param  mixed  $data
+     * @param  string  $message
+     * @return JsonResponse
+     */
+    public static function conflictResponse(mixed $data, string $message = ''): JsonResponse
+    {
+        return self::errorResponse($data, $message, Response::HTTP_CONFLICT);
+    }
+
+    /**
+     * Response with status code 422.
+     *
+     * @param  mixed  $data
+     * @param  string  $message
+     * @return JsonResponse
+     */
+    public static function unprocessableResponse(mixed $data, string $message = ''): JsonResponse
+    {
+        return self::errorResponse($data, $message, Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
